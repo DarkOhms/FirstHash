49fe514ee776f1b78eb1b1ce717e0b4b62eba489
Luke Martin <luke_martin45@yahoo.com>
2017-11-05 22:28:30 -0800

    this prime algorithm is not working yet

--- src/FirstHash.java
+++ src/FirstHash.java
@@ -1,9 +1,16 @@
 
 public class FirstHash {
+	
   static int HASH_SIZE = 31;
-  int[] hash = new int[HASH_SIZE];
+  hashItem<T>[] hash = new hashItem<T>[HASH_SIZE];
+  
   
   int hash(key) {
 	  return key%HASH_SIZE;
   }
+}
+
+protected class hashItem<T extends Comparable <T>> {
+  int key;
+  T data;
 }
--- src/Prime.java
+++ src/Prime.java
@@ -4,6 +4,7 @@ public class Prime {
 
 	static final int MAX_PRIME = 100;
 	static int floor = 7;
+	static int nextPrime;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -14,17 +15,43 @@ public class Prime {
 			primes.add(3);
 			primes.add(5);
 			primes.add(7);
+			primes.add(11);
+			
+		int thisFloor = primes.getLast();
+		nextPrime = thisFloor;
 		
-		while(floor < MAX_PRIME) {
+		thisFloor++;
+		while(thisFloor < MAX_PRIME) {
 		  
-		  for( Integer e: primes ) {	
+		//print the list
+		  for( Integer e: primes ) {
 			System.out.println(e);
 		  }
-		  int temp = 8/7;
-		  System.out.println(temp);
-		  floor = 100;
+		  
+		  int maxToTest = (int)Math.sqrt((double)thisFloor);
+		  System.out.println("Maximum prime to test through: " + maxToTest);
+		  for(int i = 1; primes.get(i) < maxToTest;) {
+			int testResult;
+			int primeToCheck = primes.get(i);
+			testResult = thisFloor%primes.get(i);
+			
+			if(testResult == 0) {
+				thisFloor++;
+				maxToTest = (int)Math.sqrt((double)thisFloor);
+				i=1;
+			}else {
+				i++;
+			}
+		  }
+		  nextPrime = thisFloor;
+		  primes.add(nextPrime);
+		  System.out.println("The next prime is: " + nextPrime);
+		  thisFloor++;
+		 
+		  
 		}
 
 	}
+	
 
 }
